48a7144419efe1747b6067c35390120531bcbe83
Carlos Gomes <up201906622@fe.up.pt>
2021-03-19 17:59:21 +0000

    Utils and Signals documentation

--- src/signals.h
+++ src/signals.h
@@ -7,13 +7,34 @@
 #include <stdbool.h>
 #include "register.h"
 
-
+/**
+ * @brief Saves the input Fich/Dir by the user in the variable char* path
+ * 
+ * @param fichDir 
+ */
 void getFichDir(const char *fichDir);
 
+/**
+ * @brief Saves in the event's register file the info when SIGTERM is received (when the user asks the programm to end) and terminates the process
+ * 
+ * @param sig 
+ */
 void sigtermHandler(int sig);
-
+/**
+ * @brief Saves in the event's register file the info when SIGCONT is received (when the user asks the programm to continue)
+ * 
+ * @param sig 
+ */
 void sigcontHandler(int sig);
-
+/**
+ * @brief Process the signal SIGINT, when the user presses CTRL-C
+ * 
+ * @param sig 
+ */
 void sigHandler(int sig);
-
+/**
+ * @brief Saves in the event's register file the info of other signals
+ * 
+ * @param sig 
+ */
 void sigHandlerDefault(int sig);
--- src/utils.h
+++ src/utils.h
@@ -6,10 +6,20 @@
 #include <sys/times.h>
 #include <string.h>
 #include <stdbool.h>
-extern clock_t start;
-extern long ticks;
-extern struct tms *buf;
-
+extern clock_t start;    // Type capable of represents the process running time of implementation
+extern long ticks;       // Number of clock ticks per second
+extern struct tms *buf;  // describes CPU time used by a process and its children
+/**
+ * @brief This function receives a decimal number and converts it to the corresponding octal number
+ * 
+ * 
+ * @param decimalNumber 
+ * @return int 
+ */
 int convertDecimalToOctal(int decimalNumber);
 
+/**
+ * @brief Stars the clock and saves the number of clock ticks per second
+ * 
+ */
 void init_clock();
