3fad1dcbfa7da353cc731b440b45026510f2fc2d
H0wl3r2001 <afonso.dc.monteiro@outlook.pt>
2021-03-19 18:13:14 +0000

    Last documentation

--- src/register.h
+++ src/register.h
@@ -18,13 +18,42 @@ enum event{PROC_CREAT, PROC_EXIT, SIGNAL_RECV, SIGNAL_SENT, FILE_MODF};
 extern struct tms *buf;
 extern clock_t start,mid,end;
 extern long ticks;
-
+/**
+ * @brief Reads the path passed in the parameter.
+ * 
+ * @param envp 
+ */
 void env_path(char *envp[]);
-
+/**
+ * @brief Registers an event without signal, saving the time it took to process.
+ * 
+ * @param event 
+ * @param pid 
+ * @param envp 
+ * @param argv 
+ * @param argc 
+ * @param after_buf 
+ * @param before_buf 
+ */
 void mke_register_wout_signal(enum event event,  pid_t pid, char *envp[], char* argv[], int argc, struct stat after_buf,struct stat before_buf);
-
+/**
+ * @brief Registers an event with signal, saving the time it took to process.
+ * 
+ * @param event 
+ * @param pid 
+ * @param signo 
+ * @param exit_c 
+ */
 void mke_register_w_signal(enum event event,  pid_t pid, int signo, int exit_c);
-
+/**
+ * @brief Opens file ("parent" process).
+ * 
+ * @param envp 
+ */
 void init_file(char *envp[]);
-
+/**
+ * @brief Opens file ("Child" process).
+ * 
+ * @param envp 
+ */
 void init_file_children(char *envp[]);
